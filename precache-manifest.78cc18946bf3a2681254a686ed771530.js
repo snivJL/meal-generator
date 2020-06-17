4a992f6db68743fd1d3b199a7a35208cb4fd074c
snivJL <julien.lejay@gmail.com>
2020-06-17 14:34:42 +0700

    Updates

---
+++ precache-manifest.78cc18946bf3a2681254a686ed771530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a52590a5daa90cb2afba4138cfda7e",
+    "url": "/meal-generator/index.html"
+  },
+  {
+    "revision": "4be2721163741709071b",
+    "url": "/meal-generator/static/css/main.3ceaaf4f.chunk.css"
+  },
+  {
+    "revision": "6426192d613afa546d60",
+    "url": "/meal-generator/static/js/2.d396a834.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/meal-generator/static/js/2.d396a834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be2721163741709071b",
+    "url": "/meal-generator/static/js/main.53b68cff.chunk.js"
+  },
+  {
+    "revision": "3c7b164bb8a0b492fa3d",
+    "url": "/meal-generator/static/js/runtime-main.5536caa2.js"
+  }
+]);
